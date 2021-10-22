2009b7dbc6372bdf5e73410d0a672cdd8220428e
Marc Hildenbrand <mhilde@canva.com>
2021-10-22 03:04:31 +0000

    Try sns resource

--- hello-chalice/app.py
+++ hello-chalice/app.py
@@ -11,10 +11,19 @@ def index():
 def foo():
     return {'bar': 'baz'}
 
-@app.on_s3_event(bucket='mwh-burner-chalice-testing')
-def handler(event):
-    print("Object uploaded for bucket: %s, key: %s"
-          % (event.bucket, event.key))
+@app.on_sns_message(topic='MyDemoTopic')
+def handle_sns_message(event):
+    app.log.debug("Received message with subject: %s, message: %s",
+                  event.subject, event.message)
+
+
+# NOTE: s3_events are not supported by package function and thus can only
+# be done through Chalice deploy
+# @app.on_s3_event(bucket='mwh-burner-chalice-testing')
+# def handler(event):
+#     print("Object uploaded for bucket: %s, key: %s"
+#           % (event.bucket, event.key))
+
 # The view function above will return {"hello": "world"}
 # whenever you make an HTTP GET request to '/'.
 #
@@ -25,12 +34,12 @@ def handler(event):
 #    # '/hello/james' -> {"hello": "james"}
 #    return {'hello': name}
 #
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
+@app.route('/users', methods=['POST'])
+def create_user():
+    # This is the JSON body the user sent in their POST request.
+    user_as_json = app.current_request.json_body
+    # We'll echo the json body back to the user in a 'user' key.
+    return {'user': user_as_json}
 #
 # See the README documentation for more examples.
 #
